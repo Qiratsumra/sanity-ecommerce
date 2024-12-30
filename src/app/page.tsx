4a28c57097892a3d3ddf1b72e2f58962521962cc
Qiratsumra <saeedqirat43@gmail.com>
2024-12-30 19:40:19 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import ProductList from "./components/ProductList";
-import AddCart from "./components/AddCart";
-
 export default function Home() {
   return (
 <div>
--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -3,14 +3,19 @@ import { structureTool} from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemas } from "./schema/main";
 
+const projectId:any = process.env.NEXT_SANITY_PROJECT_ID
+const dataset:any =process.env.NEXT_SANITY_DATASET
+
+
 export const SanityConfig =defineConfig({
     name:'default',
     title:'/studio',
-    projectId:'vursk5ej',
-    dataset:'production',
+    projectId:projectId,
+    dataset:dataset,
     plugins:[structureTool(),visionTool()],
     basePath:'/studio',
     schema:{
         types:schemas
     }
+    
 })
