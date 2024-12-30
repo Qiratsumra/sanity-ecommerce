74ec92973077467f5cce77fa8a10279da45c7779
Qiratsumra <saeedqirat43@gmail.com>
2024-12-30 19:46:10 +0500

    first commit

--- src/sanity/sanity.client.ts
+++ src/sanity/sanity.client.ts
@@ -1,8 +1,11 @@
 import { createClient, type ClientConfig } from "next-sanity";
 
+const projectId = process.env.NEXT_SANITY_PROJECT_ID
+const dataset =process.env.NEXT_SANITY_DATASET
+
 const SanityClient:ClientConfig = {
-    projectId:'vursk5ej',
-    dataset:'production',
+    projectId:projectId,
+    dataset:dataset,
     apiVersion:'2024-12-28',
     useCdn:true
 }
